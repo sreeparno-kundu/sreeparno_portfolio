e4cbaf534e52a197b3d9ea5d3b6a65bf71ddfc05
Sreeparno-Kundu <143952678+sreeparno-kundu@users.noreply.github.com>
2024-03-02 17:11:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
+
